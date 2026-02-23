444bab942891618b8b4b89a3b9ecd07785a839ed
AnatoliySokolov98 <anatoliysokolov98@gmail.com>
2026-02-22 22:29:32 -0800

    segtree

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod segment_tree;
+use segment_tree::SegmentTree;
+
+fn main() {
+    
+}
--- src/segment_tree.rs
+++ src/segment_tree.rs
@@ -1,4 +1,4 @@
-struct SegmentTree {
+pub struct SegmentTree {
     nums: Vec<i32>,
     length: usize,
 }
@@ -7,7 +7,7 @@ impl SegmentTree {
     pub fn new(length: usize) -> Self {
         SegmentTree {
             nums: vec![0; length * 4],
-            length,
+            length: length,
         }
     }
 
@@ -20,77 +20,76 @@ impl SegmentTree {
         res
     }
 
-    fn build(
-        &mut self,
-        nums: &[i32],
-        current_index: usize,
-        left_bound: usize,
-        right_bound: usize,
-    ) {
+    fn build(&mut self, nums: &[i32], curr_index: usize, left_bound: usize, right_bound: usize) {
         if left_bound == right_bound {
-            self.nums[current_index] = nums[left_bound];
+            self.nums[curr_index] = nums[left_bound];
             return;
         }
+
         let pivot = (left_bound + right_bound) / 2;
-        let left_index = current_index * 2 + 1;
-        let right_index = current_index * 2 + 2;
+        let left_index = curr_index * 2 + 1;
+        let right_index = curr_index * 2 + 2;
+
         self.build(nums, left_index, left_bound, pivot);
         self.build(nums, right_index, pivot + 1, right_bound);
-        self.nums[current_index] = self.nums[left_index] + self.nums[right_index];
+        self.nums[curr_index] = self.nums[left_index] + self.nums[right_index];
     }
 
     pub fn update(&mut self, target_index: usize, val: i32) {
-        self.update_helper(target_index, val, 0, 0, self.length - 1)
+        self.update_helper(target_index, val, 0, 0, self.length - 1);
     }
 
     fn update_helper(
         &mut self,
         target_index: usize,
         val: i32,
-        current_index: usize,
+        curr_index: usize,
         left_bound: usize,
         right_bound: usize,
     ) {
         if left_bound == right_bound {
-            self.nums[current_index] = val;
+            self.nums[curr_index] = val;
             return;
         }
+
         let pivot = (left_bound + right_bound) / 2;
-        let left_index = current_index * 2 + 1;
-        let right_index = current_index * 2 + 2;
+        let left_index = curr_index * 2 + 1;
+        let right_index = curr_index * 2 + 2;
         if target_index <= pivot {
             self.update_helper(target_index, val, left_index, left_bound, pivot);
         } else {
             self.update_helper(target_index, val, right_index, pivot + 1, right_bound);
         }
-        self.nums[current_index] = self.nums[left_index] + self.nums[right_index];
+        self.nums[curr_index] = self.nums[left_index] + self.nums[right_index];
     }
 
-    pub fn query(&self, target_left: usize, target_right: usize) -> i32 {
-        self.query_helper(target_left, target_right, 0, 0, self.length - 1)
+    pub fn query(&self, left_target: usize, right_target: usize) -> i32 {
+        self.query_helper(left_target, right_target, 0, 0, self.length - 1)
     }
 
     fn query_helper(
         &self,
-        target_left: usize,
-        target_right: usize,
-        current_index: usize,
+        left_target: usize,
+        right_target: usize,
+        curr_index: usize,
         left_bound: usize,
         right_bound: usize,
     ) -> i32 {
-        if target_left > right_bound || target_right < left_bound {
+        if left_target <= left_bound && right_target >= right_bound {
+            return self.nums[curr_index];
+        }
+        if left_target > right_bound || right_target < left_bound {
             return 0;
         }
-        if target_left <= left_bound && target_right >= right_bound {
-            return self.nums[current_index];
-        }
+
         let pivot = (left_bound + right_bound) / 2;
-        let left_index = current_index * 2 + 1;
-        let right_index = current_index * 2 + 2;
-        self.query_helper(target_left, target_right, left_index, left_bound, pivot)
+        let left_index = curr_index * 2 + 1;
+        let right_index = curr_index * 2 + 2;
+
+        self.query_helper(left_target, right_target, left_index, left_bound, pivot)
             + self.query_helper(
-                target_left,
-                target_right,
+                left_target,
+                right_target,
                 right_index,
                 pivot + 1,
                 right_bound,
